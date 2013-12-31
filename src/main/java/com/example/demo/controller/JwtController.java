65dd56be2d4c4ffb41a750f049334ab0b91c733f
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    generateToken implemented and authenticationManager, customUserDetailService and jwtUtil declared, all at JwtController.

--- src/main/java/com/example/demo/controller/JwtController.java
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -1,18 +1,40 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.JwtRequest;
+import com.example.demo.service.CustomUserDetailService;
+import com.example.demo.util.JwtUtil;
 
 // Responsible for creating the token for the first time
 @RestController("/api")
 public class JwtController {
 	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private CustomUserDetailService customUserDetailService;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
+	
 	@PostMapping("/generateToken")
-	public ResponseEntity<T> generateToken(@RequestBody JwtRequest jwtResquest){
+	public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtResquest){
+		// Authenticate the user
+		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtResquest.getUserName(), jwtResquest.getPassword()));
 		
+		UserDetails userDetails = customUserDetailService.loadUserByUsername(jwtResquest.getUserName());
+		
+		String jwtToken = jwtUtil.generateToken(userDetails);
+		
+		return null;
 	}
 }
