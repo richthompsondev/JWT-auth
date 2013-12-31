fc3449b77da84af501258e8416a4e20b9d887f32
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    authenticationManagerBean(), authenticationManager.authenticate(), userDetails, jwtToken and jwtResponse declared at JwtController and JwtConfig

--- src/main/java/com/example/demo/config/JwtConfig.java
+++ src/main/java/com/example/demo/config/JwtConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -51,4 +52,7 @@ public class JwtConfig extends WebSecurityConfigurerAdapter {
 		return NoOpPasswordEncoder.getInstance(); // Not use in production, just the make simple 4now
 	}
 	
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 }
--- src/main/java/com/example/demo/controller/JwtController.java
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -1,33 +1,37 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.JwtRequest;
+import com.example.demo.model.JwtResponse;
 import com.example.demo.service.CustomUserDetailService;
 import com.example.demo.util.JwtUtil;
 
 // Responsible for creating the token for the first time
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class JwtController {
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
 	@Autowired
-	private CustomUserDetailService customUserDetailService;
-	
-	@Autowired
-	private JwtUtil jwtUtil;
+    private CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 	
 	@PostMapping("/generateToken")
-	public ResponseEntity<?> generateToken(@RequestBody JwtRequest jwtResquest){
+	public ResponseEntity<JwtResponse> generateToken(@RequestBody JwtRequest jwtResquest){
 		// Authenticate the user
 		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(jwtResquest.getUserName(), jwtResquest.getPassword()));
 		
@@ -35,6 +39,9 @@ public class JwtController {
 		
 		String jwtToken = jwtUtil.generateToken(userDetails);
 		
-		return null;
+		JwtResponse jwtResponse = new JwtResponse(jwtToken);
+		
+		//return ResponseEntity.ok(jwtResponse);
+		return new ResponseEntity<JwtResponse>(jwtResponse, HttpStatus.OK);
 	}
 }
