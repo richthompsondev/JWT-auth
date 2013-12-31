f4a878ceacf014042ca819278b3daf1496e2f9f4
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    JwtAuthenticationFilter created

---
+++ src/main/java/com/example/demo/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+// Call this filter only once per request
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
