c8f0221f681462f3001fdbf78d629f8c29905b34
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    JwtRequest.java created and @RequestBody JwtRequest jwtResquest passed as parameter to generateToken() method.

--- src/main/java/com/example/demo/controller/JwtController.java
+++ src/main/java/com/example/demo/controller/JwtController.java
@@ -2,14 +2,17 @@ package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.JwtRequest;
 
 // Responsible for creating the token for the first time
 @RestController("/api")
 public class JwtController {
 	
 	@PostMapping("/generateToken")
-	public ResponseEntity<T> generateToken(){
+	public ResponseEntity<T> generateToken(@RequestBody JwtRequest jwtResquest){
 		
 	}
 }
