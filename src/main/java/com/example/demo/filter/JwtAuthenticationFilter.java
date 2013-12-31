3196b6e9191a0f8f1aae5000cdd4b6a88c15f4c3
Rich <rich.thompson.dev@gmail.com>
2014-01-01 00:00:00 +0500

    JwtAuthenticationFilter implemented and project builded

--- src/main/java/com/example/demo/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/filter/JwtAuthenticationFilter.java
@@ -7,19 +7,68 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.example.demo.service.CustomUserDetailService;
+import com.example.demo.util.JwtUtil;
 
 // Call this filter only once per request
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
+	
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Autowired
+    private JwtUtil jwtUtil;
+	
+	// Get the JWT Token from request header
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// Validate that JWT Token
+		String bearerToken = httpServletRequest.getHeader("Authorization");
+        String username = null;
+        String token = null;
 		
+      // Check if Token exist or has Bearer text
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+
+            // Extract JWT Token from bearerToken
+            token = bearerToken.substring(7);
+
+            try{
+                // Extract userName from the token
+                username = jwtUtil.extractUsername(token);
+
+                // Get userDetails for this user
+                UserDetails userDetails = customUserDetailService.loadUserByUsername(username);
+
+                // Security checks
+                if(username!=null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+                    UsernamePasswordAuthenticationToken upat = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    upat.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
+
+                    SecurityContextHolder.getContext().setAuthentication(upat);
+
+                }else {
+                    System.out.println("Invalid Token!!");
+                }
+
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }else {
+            System.out.println("Invalid Bearer Token Format!!");
+        }
+
+        // If all is well forward the filter request to the request end-point
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
 	}
-	
-	
 }
